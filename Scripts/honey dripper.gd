1165c6d70da5b51d45701361f02f9eaae1cda1e5
amendolan <amendolan@gmail.com>
2018-11-25 18:17:53 -0500

    Adding honey pot and testing honey transference

--- Scripts/honey dripper.gd
+++ Scripts/honey dripper.gd
@@ -1,26 +1,30 @@
 extends Node2D
 
 var honey_scale = 4
-var honey_left = 4
+var honey_left = 0
 
-func ready():
+var animation_name = "default"
+var player_body = "bear_spider"
+
+func _ready():
     honey_left = honey_scale
-    #get_node("Area2D").connect("body_enter", self, "_collect_honey")
+    get_node("Area2D").connect("body_entered", self, "_collect_honey")
     
 func _collect_honey(body):
-    if body.get_name() != "bear_spider":
+    if body.get_name() != player_body:
         pass
         
     if honey_left == 0:
         pass
         
     var owner = get_owner()
-    if owner != null && !owner.has_honey:
-        owner.has_honey = true
-        honey_left = honey_left - 1
-        if honey_scale / 2 == honey_left:
-            var animation = get_node("AnimatedSprite")
-            honey_scale = honey_left
-            var current_frame = animation.get_frame()
-            if animation.get_hframes() > current_frame:
-                animation.set_frame(current_frame + 1)
+    if owner != null || owner.has_honey:
+        pass
+        
+    owner.has_honey = true
+    honey_left = honey_left - 1
+    if (honey_scale / 2 >= honey_left || honey_left == 0):
+        var animation = get_node("AnimatedSprite")
+        honey_scale = honey_left
+        if animation.frames.get_frame_count(animation_name) > animation.frame:
+            animation.frame = animation.frame + 1
---
+++ Scripts/honey pot.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+var honey_added = 0
+var honey_total = 4
+
+var animation_name = "default"
+var player_body = "bear_spider"
+
+func _ready():
+    get_node("Area2D").connect("body_entered", self, "_add_honey")
+    
+func _add_honey(body):
+    print(body.get_name())
+    
+    if body.get_name() != player_body:
+        pass
+        
+    if honey_added == honey_total:
+        pass
+        
+    var owner = get_owner()
+    if owner == null || !owner.has_honey:
+        pass
+        
+    honey_added = honey_added + 1
+    
+    var update_frame = (honey_total / 2 == honey_added) || (honey_total == honey_added)
+    if update_frame:
+        var animation = get_node("AnimatedSprite")
+        if animation.frames.get_frame_count(animation_name) > animation.frame:
+            animation.frame = animation.frame + 1
+            
+    owner.has_honey = false
+        
